b49c68f2677f84db830f50c1be85f0dd7d4967c9
amandatavares <amandatvt@gmail.com>
2019-06-12 13:48:33 -0300

    Initial Commit

---
+++ boredApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  boredApp
+//
+//  Created by Amanda Tavares on 12/06/19.
+//  Copyright © 2019 Amanda Tavares. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
